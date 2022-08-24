cf20bae8467281c140bceb3eaa6a475766c787ef
VeronikaSergeevna <veronika.sergeevna26@gmail.com>
2022-08-25 00:28:29 +0200

    some changes for api

--- JS_for_project.js
+++ JS_for_project.js
@@ -59,9 +59,8 @@ form.addEventListener("submit", searchFor);
 // api
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = `4be40cda2262e1bbda336fc98ac66a97`;
+  let apiKey = "4be40cda2262e1bbda336fc98ac66a97";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -160,5 +159,3 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
 }
-
-displayForecast();
